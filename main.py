8532fc0ab44a045ef2623392be6cb68e2e2917e3
ngntrgduc <trungducnguyen1511@gmail.com>
2023-04-20 10:21:17 +0700

    Remove descriptions

--- main.py
+++ main.py
@@ -10,13 +10,11 @@ news = [_.text for _ in raw]
 links = [_.attrs['href'] for _ in raw]
 dates = [_.text.replace('\t', '').strip() 
          for _ in soup.find_all(class_='mod-articles-category-date')]
-descriptions = [_.text.replace('\r', '').replace('\t', '').replace('\n', '')
-                for _ in soup.find_all(class_='mod-articles-category-introtext')]
 
 file = Path('README.md').rename(Path('README.md').with_suffix('.txt'))
 with open(file, 'w', encoding='utf-8') as f:
     for i, new in enumerate(news):
-        f.write(f' - **{dates[i]}** - [{new}](https://math.hcmus.edu.vn/{links[i]}): {descriptions[i]}\n')
+        f.write(f' - **{dates[i]}** - [{new}](https://math.hcmus.edu.vn/{links[i]})\n')
     
 f.close()
 
