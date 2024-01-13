7c5f11dcbf889668849e32b886c9144a5099dc76
ngntrgduc <trungducnguyen1511@gmail.com>
2024-01-13 10:45:22 +0700

    Bold and italic latest post, easier to check

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from bs4 import BeautifulSoup as bs
 import requests
+import re
 
 page = requests.get('https://math.hcmus.edu.vn/')
 soup = bs(page.content, features='lxml') 
 
 raw   = [_ for _ in soup.find_all(class_='mod-articles-category-title')]
-news  = [_.text for _ in raw]
+news  = [_.text.strip() for _ in raw]
 links = [_.attrs['href'] for _ in raw]
 dates = [_.text.replace('\t', '').strip() 
          for _ in soup.find_all(class_='mod-articles-category-date')]
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    old_data = f.read()
+    old_link = re.findall(r'http.*\w', old_data)
+
 with open('README.md', 'w', encoding='utf-8') as f:
-    for i, new in enumerate(news):
-        f.write(f' - **{dates[i]}** - [{new}](https://math.hcmus.edu.vn{links[i]})\n')
+    for i, title in enumerate(news):
+        link = f'https://math.hcmus.edu.vn{links[i]}'
+        if link not in old_link:
+            f.write(f' - **{dates[i]}** - [***{title}***]({link})\n')
+        else:
+            f.write(f' - **{dates[i]}** - [{title}]({link})\n')
