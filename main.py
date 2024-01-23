12374da705381568f4f417730c872cb9283bad3f
ngntrgduc <trungducnguyen1511@gmail.com>
2024-01-23 09:20:19 +0700

    Make sure the post with updated date is bolded

--- main.py
+++ main.py
@@ -13,13 +13,18 @@ dates = [_.text.replace('\t', '').strip()
 
 with open('README.md', 'r', encoding='utf-8') as f:
     old_data = f.read()
+    old_date = [line[5:15] for line in old_data.splitlines()]
     old_link = re.findall(r'http.*\)', old_data)
     old_link = [link[:-1] for link in old_link]
+    
+    data = {}
+    for link, date in zip(old_link, old_date):
+        data[link] = date
 
 with open('README.md', 'w', encoding='utf-8') as f:
     for i, title in enumerate(news):
         link = f'https://math.hcmus.edu.vn{links[i]}'
-        if link not in old_link:
+        if link not in old_link or dates[i] != data[link]:
             f.write(f' - **{dates[i]}** - [***{title}***]({link})\n')
         else:
             f.write(f' - **{dates[i]}** - [{title}]({link})\n')
