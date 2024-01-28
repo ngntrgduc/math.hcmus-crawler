568aa73e1a41937baea28220367fca0f4e79ec1c
ngntrgduc <trungducnguyen1511@gmail.com>
2024-01-28 11:12:22 +0700

    Change format when update

--- main.py
+++ main.py
@@ -11,20 +11,24 @@ links = [_.attrs['href'] for _ in raw]
 dates = [_.text.replace('\t', '').strip() 
          for _ in soup.find_all(class_='mod-articles-category-date')]
 
+data = {}
+
 with open('README.md', 'r', encoding='utf-8') as f:
     old_data = f.read()
     old_date = [line[5:15] for line in old_data.splitlines()]
     old_link = re.findall(r'http.*\)', old_data)
     old_link = [link[:-1] for link in old_link]
     
-    data = {}
     for link, date in zip(old_link, old_date):
         data[link] = date
 
 with open('README.md', 'w', encoding='utf-8') as f:
     for i, title in enumerate(news):
         link = f'https://math.hcmus.edu.vn{links[i]}'
-        if link not in old_link or dates[i] != data[link]:
-            f.write(f' - **{dates[i]}** - [***{title}***]({link})\n')
+        if link not in old_link:
+            if dates[i] != data[link]:
+                f.write(f' - **{dates[i]}** - [***{title}***]({link})\n')
+            else:
+                f.write(f' - **{dates[i]}** - [{title}]({link})\n')   
         else:
             f.write(f' - **{dates[i]}** - [{title}]({link})\n')
