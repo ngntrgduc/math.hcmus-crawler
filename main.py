fd93b9ad88a3c65263511d7936824bf0a02fd786
ngntrgduc <trungducnguyen1511@gmail.com>
2024-02-21 14:34:21 +0700

    Remove bold new links and refactor
    Because it depend on the current README file so if the README file broken, the script will be broken to. Keep it simple

--- main.py
+++ main.py
@@ -1,34 +1,15 @@
 from bs4 import BeautifulSoup as bs
 import requests
-import re
 
 page = requests.get('https://math.hcmus.edu.vn/')
 soup = bs(page.content, features='lxml') 
 
 raw   = [_ for _ in soup.find_all(class_='mod-articles-category-title')]
 news  = [_.text.strip() for _ in raw]
-links = [_.attrs['href'] for _ in raw]
-dates = [_.text.replace('\t', '').strip() 
+links = [f'https://math.hcmus.edu.vn{_.attrs["href"]}' for _ in raw]
+dates = [_.text.replace('\t', '').strip()
          for _ in soup.find_all(class_='mod-articles-category-date')]
 
-data = {}
-
-with open('README.md', 'r', encoding='utf-8') as f:
-    old_data = f.read()
-    old_date = [line[5:15] for line in old_data.splitlines()]
-    old_link = re.findall(r'http.*\)', old_data)
-    old_link = [link[:-1] for link in old_link]
-    
-    for link, date in zip(old_link, old_date):
-        data[link] = date
-
 with open('README.md', 'w', encoding='utf-8') as f:
-    for i, title in enumerate(news):
-        link = f'https://math.hcmus.edu.vn{links[i]}'
-        if link not in old_link:
-            if dates[i] != data[link]:
-                f.write(f' - **{dates[i]}** - [***{title}***]({link})\n')
-            else:
-                f.write(f' - **{dates[i]}** - [{title}]({link})\n')   
-        else:
-            f.write(f' - **{dates[i]}** - [{title}]({link})\n')
+    for date, title, link in zip(dates, news, links):
+        f.write(f' - **{date}** - [{title}]({link})\n')
